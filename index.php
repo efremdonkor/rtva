bd0f951e2cd69355c663c8b04a23a10cbcb8ad8b
MaxwellDonkor <MaxwellDonkor@10.0.0.141>
2017-07-18 20:40:20 -0400

    start afternoon

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Restoration Temple, Dumfries, Virginia :: ICGC - The International Central Gospel Church
+        </title>
+
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <!--        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+                <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css.map">
+                <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.min.css">
+                <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.min.css.map">
+                <link type="text/css" rel="stylesheet" href="css/bootstrap.css.map">
+                <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+                <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css.map">-->
+        <link href="css/styles.css" type="text/css" rel="stylesheet">
+        <script
+            src="https://code.jquery.com/jquery-3.2.1.js"
+            integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+        crossorigin="anonymous"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+
+        <div class="site-container">
+            <div class="container-fluid" id="holdnav">
+            <nav class="navbar navbar-inverse container">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span> 
+                        </button>
+                        <a class="navbar-brand" href="#">Restoration Temple</a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <!--                            <ul class="nav navbar-nav">
+                                                        <li class="active"><a href="#">Home</a></li>
+                                                        <li><a href="#">Page 1</a></li>
+                                                        <li><a href="#">Page 2</a></li> 
+                                                        <li><a href="#">Page 3</a></li> 
+                                                    </ul>-->
+                        <ul class="nav navbar-nav navbar-right" id="navigate">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">New Here?</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Ministries</a></li>
+                            <li><a href="#">Media</a></li>
+                            <li><a href="#">Contact US</a></li>
+<!--                            <span class="glyphicon glyphicon-user"></span> 
+                            <span class="glyphicon glyphicon-log-in"></span> -->
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+                </div>
+        </div>
+    </body>
+</html>
